fdeb5fbeaa360d285b473ced6ab64b360a91eee1
Elaine <sosohn@gmail.com>
2023-02-17 13:18:58 -0800

    test for post

--- app/routes/watchlist_routes.py
+++ app/routes/watchlist_routes.py
@@ -35,3 +35,71 @@ def delete_one_watchlist(viewer_id):
     db.session.commit()
     
     return make_response(jsonify(watchlist.to_dict()), 200)
+
+# @watchlist_bp.route("/<viewer_id>/watchlist/add", methods=["POST"])  #{params: {"viewer_id":id , "content":content}}
+# def add_one_content_to_viewer_watchlist(viewer_id, content):
+#     request_body = request.get_json()
+#     request_content = validate_request_body(Content, request_body["content"])
+
+#     viewer = validate_model(Viewer, request_body["viewer_id"])
+#     # content = validate_model(Content, request_content["content_id"])
+#     content = Content.query.get(request_content["content_id"])
+
+#     if not content:
+#         new_content = content
+#         db.session.add(new_content)
+#         db.session.commit()
+
+#     new_watchlist = {}
+#     new_watchlist["viewer_id"] = viewer.id,
+#     new_watchlist["content_id"] = content.id
+        
+
+#     db.session.add(new_watchlist)
+#     db.session.commit()
+
+#     return make_response(jsonify(f"Watchlist {new_watchlist.id} successfully created"), 201)
+
+# @watchlist_bp.route("/test", methods=["POST"])  #{params: {"viewer_id":id , "content":content}}
+# def add_one_content_to_viewer_watchlist():
+#     request_body = request.get_json()
+        
+#     new_content = Content(
+#             poster = request_body["poster"],
+#             title = request_body["title"],
+#             date = request_body["date"],
+#             media_type = request_body["media_type"],
+#             vote_average = request_body["vote_average"]
+#         )
+#     console.log(new_content)
+
+#     db.session.add(new_content)
+#     db.session.commit()
+#     console.log(Content.query.all())
+#     # new_watchlist = {}
+#     # new_watchlist["viewer_id"] = viewer.id,
+#     # new_watchlist["content_id"] = request_content.id
+        
+
+#     # db.session.add(new_watchlist)
+#     # db.session.commit()
+
+#     return make_response(jsonify(f"successfully created"), 201)
+
+@watchlist_bp.route("/add", methods=["POST"])
+def create_watchlist():
+    request_body = validate_request_body(Content, request.get_json())
+    
+    new_content = Content(
+            poster = request_body["poster"],
+            title = request_body["title"],
+            date = request_body["date"],
+            media_type = request_body["media_type"],
+            vote_average = request_body["vote_average"]
+        )
+    console.log(new_content)
+
+    db.session.add(new_content)
+    db.session.commit()
+
+    return make_response(jsonify(f"Watchlist {new_watchlist.id} successfully created"), 201)
