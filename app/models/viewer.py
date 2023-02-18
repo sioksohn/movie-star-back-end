f2c69a07a6a287292536ecc3b4d16b07e190befb
Elaine <sosohn@gmail.com>
2023-02-17 21:39:15 -0800

    change viewer

--- app/models/viewer.py
+++ app/models/viewer.py
@@ -4,7 +4,7 @@ class Viewer(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, nullable=False)
+    password = db.Column(db.String)
     watchlists = db.relationship("Watchlist", back_populates="viewer") #contents
 
     def to_dict(self):
