75ae84f093ae74ffb12a9079a7588ee24f49cd91
Elaine <sosohn@gmail.com>
2023-02-17 21:47:46 -0800

    remove all migrations

--- app/models/viewer.py
+++ app/models/viewer.py
@@ -4,7 +4,7 @@ class Viewer(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
-    password = db.Column(db.String)
+    password = db.Column(db.String, nullable=False)
     watchlists = db.relationship("Watchlist", back_populates="viewer") #contents
 
     def to_dict(self):
